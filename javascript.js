c877aafbe3edcebede7ee78a3dc706522b22dc77
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-09-22 11:22:56 -0500

    Add files via upload
    ADDED PAGES

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function calbill() {
+    const Total=document.getElementById("total").value;
+    const Rate=document.getElementById("rate").value;
+    const Tip= parseFloat(Total)*parseFloat(Rate)/100;
+    document.getElementById("tip").innerHTML=Tip.toFixed(2);
+    const Tax = 5.5/100*parseFloat(Total);
+    document.getElementById("tax").innerHTML= Tax.toFixed(2);
+    const grndtota = parseFloat(Total)+Tip+Tax;
+    document.getElementById("grand").innerHTML=grndtota.toFixed(2);
+}
